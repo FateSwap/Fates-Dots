9b44f62767e9bd688bd50f93b947b9f6e5043258
Fate <35616898+FateSwap@users.noreply.github.com>
2025-06-01 22:01:04 -0400

    Basic | based on tonybanters config

---
+++ nixos/home.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+    # Base settings ( from tonybanters video )
+    home.username = "spaceman";
+    home.homeDirectory = "/home/spaceman";
+    home.stateVersion = "25.05";
+
+    # Testing
+
+}
+
